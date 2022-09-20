98a244cd181720642bb9d76e6d61e4f3589f3f19
maged-sbs <102479398+Maged-Samir@users.noreply.github.com>
2022-09-20 23:44:03 +0200

    Read-Update-Delete
    here , we finished another operation
    (Read - Update - Delete ) and also dealing with X.PagedList to create simple pagination

--- Squad/Squad/Controllers/ClubsController.cs
+++ Squad/Squad/Controllers/ClubsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Squad.Models;
 using Squad.ViewModels;
+using X.PagedList;
 
 namespace Squad.Controllers
 {
@@ -16,9 +17,10 @@ namespace Squad.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public  IActionResult Index(int? page)
         {
-            var clubs = await _context.Clubs.OrderByDescending(m => m.Rate).ToListAsync();
+            var clubs =  _context.Clubs.OrderByDescending(m => m.Rate).ToList().ToPagedList(page ?? 1, 5);
+            // clubs.ToPagedList(page ?? 1, 9);
             return View(clubs);
         }
 
@@ -91,8 +93,124 @@ namespace Squad.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return BadRequest();
 
-       
+            var movie = await _context.Clubs.FindAsync(id);
+
+            if (movie == null)
+                return NotFound();
+
+            var viewModel = new ClubFormViewModel
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                ShortName=movie.ShortName,
+                Stadium=movie.Stadium,
+                WebSite=movie.WebSite,
+                LeagueId = movie.LeagueId,
+                Rate = movie.Rate,
+                Year = movie.Year,
+                Titles = movie.Titles,
+                Logo = movie.Logo,
+                Leagues = await _context.Leagues.OrderBy(m => m.Name).ToListAsync()
+            };
+
+            return View("ClubForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ClubFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Leagues = await _context.Leagues.OrderBy(m => m.Name).ToListAsync();
+                return View("ClubForm", model);
+            }
+
+            var club = await _context.Clubs.FindAsync(model.Id);
+
+            if (club == null)
+                return NotFound();
+
+            var files = Request.Form.Files;
+
+            if (files.Any())
+            {
+                var poster = files.FirstOrDefault();
+
+                using var dataStream = new MemoryStream();
+
+                await poster.CopyToAsync(dataStream);
+
+                model.Logo = dataStream.ToArray();
+
+                if (!_allowedExtenstions.Contains(Path.GetExtension(poster.FileName).ToLower()))
+                {
+                    model.Leagues = await _context.Leagues.OrderBy(m => m.Name).ToListAsync();
+                    ModelState.AddModelError("Poster", "Only .PNG, .JPG images are allowed!");
+                    return View("ClubForm", model);
+                }
+
+                if (poster.Length > _maxAllowedPosterSize)
+                {
+                    model.Leagues = await _context.Leagues.OrderBy(m => m.Name).ToListAsync();
+                    ModelState.AddModelError("Poster", "Poster cannot be more than 1 MB!");
+                    return View("ClubForm", model);
+                }
+
+                club.Logo = model.Logo;
+            }
+
+            club.Name = model.Name;
+            club.ShortName = model.ShortName;
+            club.LeagueId = model.LeagueId;
+            club.Stadium = model.Stadium;
+            club.WebSite = model.WebSite;
+            club.Year = model.Year;
+            club.Rate = model.Rate;
+            club.Titles = model.Titles;
+            
+
+            _context.SaveChanges();
+
+            //_toastNotification.AddSuccessToastMessage("Movie updated successfully");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var club = await _context.Clubs.Include(m => m.League).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (club == null)
+                return NotFound();
+
+            return View(club);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var club = await _context.Clubs.FindAsync(id);
+
+            if (club == null)
+                return NotFound();
+
+            _context.Clubs.Remove(club);
+            _context.SaveChanges();
+
+            return Ok();
+
+        }
 
 
     }
